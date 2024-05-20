dab9e090a428a474d25dbd297dc16204078d193b
Roberto Perez <robj.perez@gmail.com>
2024-05-21 00:41:24 +0200

    Fixed enemy movement. You can now shoot at mushrooms!

--- src/main.scm
+++ src/main.scm
@@ -1,5 +1,6 @@
 (use-modules (raylib)
-	     (srfi srfi-27)) ; For array procedures
+	     (srfi srfi-27)
+	     (ice-9 format)) ; For array procedures
 
 (define CELL-MUSHROOM 1)
 (define CELL-EMPTY 0)
@@ -40,7 +41,11 @@
 (define grid-data (make-array CELL-EMPTY grid-height grid-width))
 
 ;; Utilities
-(define (pos-to-grid pos) (list (floor/ (car pos) 16) (floor/ (cadr pos) 16)))
+(define (pos-to-grid pos)
+  (list
+   (min (floor/ (car pos) 16) (- grid-width 1))
+   (min (floor/ (cadr pos) 16) (- grid-height 1))))
+
 (define (opposite-direction dir)
   (cond
    ((= dir DIRECTION-L) DIRECTION-R)
@@ -56,11 +61,11 @@
               (loop positions)
               (loop (cons new-pos positions)))))))
 ;; Generate 20 unique random positions
-(define random-positions (generate-random-positions (* grid-width grid-height) 10))
+(define random-positions (generate-random-positions (* grid-width grid-height) 60))
 
 ;; Fill the array with #t in the random positions
 (for-each (lambda (pos)
-            (let ((row (quotient pos grid-height))
+            (let ((row (quotient pos grid-width))
                   (col (remainder pos grid-width)))
               (array-set! grid-data CELL-MUSHROOM row col)))
           random-positions)
@@ -99,9 +104,16 @@
 	(if player-fire-active
 	    (begin
 	      (set! player-fire-position (update-player-fire-position player-fire-position))
-	      (if (< (cadr player-fire-position) 0)
-					; (set! player-fire-active 0)
-		  (set! player-fire-active #f))))
+	      (let* ((new-pos-grid (pos-to-grid player-fire-position))
+		     (grid-x (car new-pos-grid))
+		     (grid-y (cadr new-pos-grid)))
+		(cond
+		 ((< (cadr player-fire-position) 0)
+		  (set! player-fire-active #f)) ;Remove fire when goes out of screen
+		 ((= CELL-MUSHROOM (array-ref grid-data grid-y grid-x))
+		  (array-set! grid-data CELL-EMPTY grid-y grid-x)
+		  (set! player-fire-active #f)) ; Check collision with mushrooms		
+		 ))))
 
 
 	;; enemy position
@@ -110,29 +122,28 @@
 			  (e-x (vector-ref enemy 1))
 			  (e-y (vector-ref enemy 2)))
 		      (cond
-		       ((= DIRECTION-L direction) (vector-set! enemy 1 (- e-x 5)))
-		       ((= DIRECTION-R direction) (vector-set! enemy 1 (+ e-x 5))))
+		       ((= DIRECTION-L direction) (vector-set! enemy 1 (- e-x 3)))
+		       ((= DIRECTION-R direction) (vector-set! enemy 1 (+ e-x 3))))
 		      (let* ((new-pos (list (vector-ref enemy 1) (vector-ref enemy 2)))
 			     (new-pos-grid (pos-to-grid new-pos))
 			     (grid-x (car new-pos-grid))
 			     (grid-y (cadr new-pos-grid)))
 			(cond
-		
-			 ((> grid-x grid-width)
+			 ((> grid-x (- grid-width 2))
 			  (begin
-			      (display "R BOUNDS ")
+			      (display "R BOUNDS\n")
 			      (vector-set! enemy 2 (+ 16 e-y))
 			      (vector-set! enemy 0 DIRECTION-L)))
 			 ((< grid-x 0)
 			  (begin
-			    (display "L BOUNDS ")
+			    (format #t "L BOUNDS\n")
 			    (vector-set! enemy 2 (+ 16 e-y))
 			    (vector-set! enemy 0 DIRECTION-R)))
-			 ((= CELL-MUSHROOM (array-ref grid-data grid-x grid-y))
-			    (begin
-			      (display "HIT! ")
-			      (vector-set! enemy 2 (+ 16 e-y))
-			      (vector-set! enemy 0 (opposite-direction direction))))))))
+			 ((= CELL-MUSHROOM (array-ref grid-data grid-y grid-x))
+			  (begin
+			    (format #t "HIT! ~d, ~d\n" grid-x grid-y)
+			    (vector-set! enemy 2 (+ 16 e-y))
+			    (vector-set! enemy 0 (opposite-direction direction))))))))
 			
 		  enemies)
 			
@@ -151,7 +162,7 @@
 				(let ((x (* col 16))
 				      (y (* row 16)))
 				  (if (= (array-ref grid-data row col) CELL-MUSHROOM)
-				      (DrawTexture mushtex x y WHITE))))
+					(DrawTexture mushtex x y WHITE))))
 			      (iota grid-width)))
 		  (iota grid-height))
 
